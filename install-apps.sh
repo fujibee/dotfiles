0e7bd01f9f0799a730267e01ead287a9a522486a
Koichi Fujikawa <koichi@flydata.com>
2021-06-08 14:39:58 -0700

    homebrew cask has been changed the cli option

--- install-apps.sh
+++ install-apps.sh
@@ -9,7 +9,7 @@ brew tap homebrew/versions
 brew tap phinze/homebrew-cask
 brew tap homebrew/binary
  
-brew install brew-cask
+#brew install brew-cask
  
 # for dev
 brew install zsh
@@ -25,46 +25,45 @@ brew install rbenv ruby-build
 brew install yarn
 brew install pass
 
-brew cask install iterm2
-brew cask install docker
-brew cask install ngrok
+brew install --cask iterm2
+brew install --cask docker
+#brew install --cask ngrok
 brew install docker-compose
 brew install hub
 brew install git-flow
-brew cask install react-native-debugger
-brew cask install macdown
-brew cask install notion
+brew install --cask react-native-debugger
+brew install --cask macdown
+brew install --cask notion
 
 # for desktop
-brew cask install google-chrome
-brew cask install dropbox
-brew cask install evernote
-#brew cask install microsoft-office
-brew cask install google-japanese-ime
-brew cask install google-drive-file-stream
-brew cask install slack
-brew cask install zoomus
-brew cask install viscosity
-brew cask install adobe-acrobat-reader
-brew cask install adobe-creative-cloud
-brew cask install authy
-#brew cask install messenger
-#brew cask install spectacle
-brew cask install android-file-transfer
-brew cask install discord
-brew cask install kindle
+brew install --cask google-chrome
+brew install --cask dropbox
+brew install --cask evernote
+#brew install --cask microsoft-office
+brew install --cask google-japanese-ime
+brew install --cask google-drive-file-stream
+brew install --cask slack
+brew install --cask zoomus
+#brew install --cask viscosity
+brew install --cask adobe-creative-cloud
+brew install --cask authy
+#brew install --cask messenger
+#brew install --cask spectacle
+brew install --cask android-file-transfer
+brew install --cask discord
+brew install --cask kindle
 
 # pass for Chrome
 brew tap amar1729/formulae
 brew install browserpass
 
 brew tap homebrew/cask-fonts #You only need to do this once for cask-fonts
-brew cask install font-fantasque-sans-mono
+brew install --cask font-fantasque-sans-mono
 
 # printer driver
 
 brew tap homebrew/cask-drivers
-brew cask install apple-hewlett-packard-printer-drivers
+brew install --cask apple-hewlett-packard-printer-drivers
 
 # from App Store
 # Xcode
