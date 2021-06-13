067054b4f7f89ffe21c5a13c73b7ed84f994f88c
Koichi Fujikawa <fujibee@gmail.com>
2021-06-13 11:38:21 -0700

    Added stack

--- install-apps.sh
+++ install-apps.sh
@@ -54,6 +54,7 @@ brew install --cask messenger
 brew install --cask discord
 brew install --cask kindle
 brew install --cask abyssoft-teleport
+brew install --cask stack-stack
 
 # pass for Chrome
 brew tap amar1729/formulae
