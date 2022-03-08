bff48dc5aa645d56260abc691f0b13b3bbb418b4
Koichi Fujikawa <fujibee@gmail.com>
2022-03-07 23:53:59 -0800

    suppored unlink and create link again

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,7 +3,11 @@
 target=`ls rc.d/`
 cd ..
 for x in $target; do
-  ln -s .dotfiles/rc.d/$x ./.$x
+  link=./.$x
+  if [ -L $link ]; then
+    unlink $link
+  fi
+  ln -s .dotfiles/rc.d/$x $link
 done
 
 # for .oh-my-zsh
